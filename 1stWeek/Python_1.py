dcb34f09174bca6aaa3d15133cee29511f283c32
david-th-hong <david.taehwan.hong@gmail.com>
2018-04-11 21:25:16 +0900

    create a python_2

---
+++ 1stWeek/Python_1.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("hello"*40)
+print("hello"+"world")
+#print("hello"+30)
+#Traceback (most recent call last):
+#   File "Python_1.py", line 4, in <module>
+#     print("hello"+30)
+# TypeError: must be str, not int
+print("hello"+str(30))
---
+++ 1stWeek/Python_2.py
@@ -0,0 +1,20 @@
+## Read ME!!
+# 이번 파일에서는 python에서의 변수에 대한 내용암ㅋ
+# 화이팅
+
+# 파이썬은 모든것이 객체로 이뤄짐
+# type을 이용해서 type을 확인 가능
+
+# print("input value for variable a")
+# a=input()
+# print(type(a))
+# print("input으로 변수를 받으면 일단 str로 들어옴 숫자를 입력해보시오")
+# a=int(input())
+# print(type(a))
+
+for i in range(10):
+    if i > 3:
+        s = i * 2
+        print(s)
+    print(i)
+    
